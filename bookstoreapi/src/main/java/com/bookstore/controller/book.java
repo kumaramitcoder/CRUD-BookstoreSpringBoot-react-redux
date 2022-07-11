99a8406bfe4cd2a36ff7781279da37eed6a736e9
amitsah <kumaramit.talk2u@gmail.com>
2022-07-11 14:44:29 +0530

    demo controller is created

--- bookstoreapi/src/main/java/com/bookstore/controller/book.java
+++ bookstoreapi/src/main/java/com/bookstore/controller/book.java
@@ -1,4 +1,21 @@
 package com.bookstore.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class book {
+
+    @RequestMapping("hello")
+
+    // Method
+    public String amit()
+    {
+        return "Hello Amit";
+    }
+
 }
